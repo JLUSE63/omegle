a905e12f96d9bbe4b9ac2973fef121fb1608853e
JLUSE63 <68025815+JLUSE63@users.noreply.github.com>
2022-11-02 14:12:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', () => {
+  $("div.logwrapper").css("top", "50px");
+  $("div.logwrapper").css("margin-left", "1000px");
+  $("div.controlwrapper").css("margin-left", "1000px");
+
+  $("#selfvideo").css("width", "312px");
+  $("#selfvideo").css("height", "234px");
+
+  $("#othervideo").css("width", "1496px");
+  $("#othervideo").css("height", "854px");
+}); 
