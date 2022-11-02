5fbdd162d7a36b16e13680cfc3acb0ad7ace52db
JLUSE63 <68025815+JLUSE63@users.noreply.github.com>
2022-11-02 23:41:18 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 window.addEventListener('load', () => {
-  $("div.logwrapper").css("top", "50px");
-  $("div.logwrapper").css("margin-left", "1000px");
-  $("div.controlwrapper").css("margin-left", "1000px");
+  // $("div.logwrapper").css("top", "50px");
+  document.getElementsByClassName("logwrapper").style.top = "50px";
+  // $("div.logwrapper").css("margin-left", "1000px");
+  document.getElementsByClassName("logwrapper").style.margin = "0px, 0px, 0px, 1000px";
+  // $("div.controlwrapper").css("margin-left", "1000px");
+  document.getElementsByClassName("controlwrapper").style.margin = "0px, 0px, 0px, 1000px";
 
-  $("#selfvideo").css("width", "312px");
-  $("#selfvideo").css("height", "234px");
+  // $("#selfvideo").css("width", "312px");
+  document.getElementsByClassName("logwrapper").style.width = "312px";
+  // $("#selfvideo").css("height", "234px");
+  document.getElementsByClassName("logwrapper").style.height = "234px";
 
-  $("#othervideo").css("width", "1496px");
-  $("#othervideo").css("height", "854px");
-}); 
+  // $("#othervideo").css("width", "1496px");
+  document.getElementsByClassName("logwrapper").style.width = "1496px";
+  // $("#othervideo").css("height", "854px");
+  document.getElementsByClassName("logwrapper").style.height = "854px";
+});
+
+// <link rel="stylesheet" href="https://raw.githubusercontent.com/JLUSE63/omegle/index/style.css"><script src="https://code.jquery.com/jquery-3.6.1.min.js"></script><script src="https://raw.githubusercontent.com/JLUSE63/omegle/index/script.js"></script>
+
+/*
+
+thttrbdfdfvcvrrrrhr = document.createElement('script');
+efvfvrgfefsdvfgthth = document.createElement('link');
+thttrbdfdfvcvrrrrhr.src = "https://raw.githubusercontent.com/JLUSE63/omegle/index/script.js";
+efvfvrgfefsdvfgthth.href = "https://raw.githubusercontent.com/JLUSE63/omegle/index/style.css";
+document.body.appendChild(thttrbdfdfvcvrrrrhr);
+document.body.appendChild(efvfvrgfefsdvfgthth);
+
+*/
